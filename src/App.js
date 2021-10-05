ea817c8ef5b162a5eb3f84410902a12cd9d14f0a
Andrei G <andreiglumov1989@gmail.com>
2021-10-05 17:45:58 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -8,18 +8,27 @@ import {usePosts} from './components/hooks/usePosts';
 import PostService from './components/API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import {useFetching} from './components/hooks/useFetching';
+import {getPageCount, getPagesArray} from './utils/pages';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = getPagesArray(totalPages)
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
   })
 
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   useEffect(() => {
     fetchPosts()
@@ -52,6 +61,12 @@ function App() {
           ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
           : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов JavaScript" />
       }
+      <div className="page__wrapper">
+        {
+          pagesArray.map(p => <span key={p} className="page">{p}</span>
+          )
+        }
+      </div>
 
     </div>
   );
